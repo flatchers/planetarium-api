1b3e09f7ef569ca7a4a93fdd274349ed0a2c00dc
Heorhii Nadiradze <swain.volt@gmail.com>
2024-03-31 15:58:36 +0300

    ad completed ShowTheme endpoint

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from product.views import PlanetariumDomeViewSet
+from product.views import PlanetariumDomeViewSet, ShowThemeViewSet
 
 router = routers.DefaultRouter()
 router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("show_theme", ShowThemeViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
--- product/views.py
+++ product/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from product.models import PlanetariumDome
-from product.serializers import PlanetariumDomeSerializer
+from product.models import PlanetariumDome, ShowTheme
+from product.serializers import PlanetariumDomeSerializer, ShowThemeSerializer
 
 
 class PlanetariumDomeViewSet(viewsets.ModelViewSet):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumDomeSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
