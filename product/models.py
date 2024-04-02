3a2df0da97e1cd5283c27da490d7ceddf963c4e4
Heorhii Nadiradze <swain.volt@gmail.com>
2024-04-02 12:32:51 +0300

    add magic function str to models.py

--- product/models.py
+++ product/models.py
@@ -8,9 +8,15 @@ class PlanetariumDome(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.name} (rows: {self.rows}, seats: {self.seats_in_row})"
+
 
 class ShowTheme(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class AstronomyShow(models.Model):
@@ -18,11 +24,17 @@ class AstronomyShow(models.Model):
     description = models.TextField(blank=True, null=True)
     show_theme = models.ManyToManyField(ShowTheme, related_name="astronomy_shows", blank=True)
 
+    def __str__(self):
+        return f"title: {self.title}, theme: {self.show_theme}"
+
 
 class ShowSession(models.Model):
     astronomy_show = models.ForeignKey(AstronomyShow, on_delete=models.CASCADE)
     planetarium_dome = models.ForeignKey(PlanetariumDome, on_delete=models.CASCADE)
     show_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.astronomy_show}, {self.planetarium_dome} {self.show_time}"
 
 
 class Reservation(models.Model):
@@ -32,9 +44,15 @@ class Reservation(models.Model):
         on_delete=models.CASCADE
     )
 
+    def __str__(self):
+        return f"{self.user}, {self.created}"
+
 
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
     show_session = models.ForeignKey(ShowSession, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, related_name="tickets")
+
+    def __str__(self):
+        return f"{self.row}, {self.seat}, {self.show_session}, {self.reservation}"
