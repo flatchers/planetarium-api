54d3a1248a01ee99dc4aafea2d379492b81500de
Heorhii Nadiradze <swain.volt@gmail.com>
2024-04-10 13:05:08 +0300

    ad ManageUserView to user/views.py

--- user/views.py
+++ user/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
