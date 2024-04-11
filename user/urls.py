d78359031f9a49db9e96a28e6ec4fa8544a6e793
Heorhii Nadiradze <swain.volt@gmail.com>
2024-04-11 13:31:35 +0300

    ad url path for user and service

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,20 @@
-from django.contrib import admin
 from django.urls import path, include
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create")
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage")
 ]
 
 app_name = "user"
