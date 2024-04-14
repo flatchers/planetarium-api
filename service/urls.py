17509f1dd5977bcfc08f8cdfd3bc0752902d10ea
Heorhii Nadiradze <swain.volt@gmail.com>
2024-04-14 18:33:01 +0300

    ad settings.MEDIA_URL TO service.urls

--- service/urls.py
+++ service/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("api/planetarium/", include("product.urls")),
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings)
