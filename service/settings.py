3b28eeb0730c86b9319375816f9cf62303343a4d
Heorhii Nadiradze <swain.volt@gmail.com>
2024-04-25 18:30:26 +0300

    improve secret_key

--- service/settings.py
+++ service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!=umdd+8$jtu$b*nl7zqo6!t5c_^nm_k08@7=x64*30258s6c5"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-!=umdd+8$jtu$b*nl7zqo6!t5c_^nm_k08@7=x64*30258s6c5")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
